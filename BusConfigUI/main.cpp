4c2ee463ae407aab403ef9b4274eac83d1c1b8de
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-12-07 00:11:38 +0100

    Added window icon

--- BusConfigUI/main.cpp
+++ BusConfigUI/main.cpp
@@ -1,10 +1,12 @@
 #include "BusConfigUI.h"
 #include <QtWidgets/QApplication>
 #include <qstylefactory.h>
+#include <qsplashscreen.h>
 
 int main(int argc, char *argv[])
 {
    QApplication a(argc, argv);
+   a.setWindowIcon(QIcon("icons/database.png"));
    BusConfigUI w;
    w.show();
    return a.exec();
